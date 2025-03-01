9dfbaa8862f2532a0752f5d249232f2e2a9a325c
Steven Dierick <steven@wulfe.be>
2025-03-01 09:12:29 +0100

    chore: add components package

---
+++ packages/components/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: {
+                'index': resolve(__dirname, 'src/index.js'),
+            },
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: ['lit'],
+        },
+    },
+})
