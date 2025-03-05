b82d08ca4ce990ddd227c6a011eb9c07daee986a
Steven Dierick <steven@wulfe.be>
2025-03-05 19:51:40 +0100

    chore: update components

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,5 +1,7 @@
+import './accordion'
 import './accordion-item'
 import './icon'
 
+export { Accordion } from './accordion'
 export { AccordionItem } from './accordion-item'
 export { Icon } from './icon'
